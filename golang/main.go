00f4abc0f17e358ec0bf232c556797b1c5ca245c
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-14 21:51:05 -0400

    Finish countVowels function in Golang

--- golang/main.go
+++ golang/main.go
@@ -8,8 +8,6 @@ import (
 
 /*
 Text
-Pig Latin 				- Pig Latin is a game of alterations played on the English language game. To create the Pig Latin form of an English word the initial consonant sound is transposed to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay). Read Wikipedia for more information on rules.
-Count Vowels 			- Enter a string and the program counts the number of vowels in the text. For added complexity have it report a sum of each vowel found.
 Check if Palindrome 	- Checks if the string entered by the user is a palindrome. That is that it reads the same forwards as backwards like “racecar”
 Count Words in a String - Counts the number of individual words in a string. For added complexity read these strings in from a text file and generate a summary.
 Text Editor 			- Notepad style application that can open, edit, and save text documents. Optional: Add syntax highlighting and other features.
@@ -24,9 +22,11 @@ func main() {
 	// fizzBuzz()
 	// reverseString("The quick brown fox jumps over the lazy dog.")
 	// pigLatin("the quick brown fox")
+	// countVowels("The quick brown fox jumps over the lazy dog.")
 }
 
 type List []string
+
 func (l List) Contains(c rune) bool {
 	for _, x := range l {
 		if x == string(c) {
@@ -34,6 +34,27 @@ func (l List) Contains(c rune) bool {
 		}
 	}
 	return false
+}
+
+type MyString string
+
+func (s MyString) FilterChars(chars string) []string {
+	var resultList []string
+	var vowels List = strings.Split("aeiou", "")
+	for _, c := range s {
+		if vowels.Contains(c) {
+			resultList = append(resultList, string(c))
+		}
+	}
+	return resultList
+}
+
+func countVowels(text string) {
+	text = strings.ToLower(text)
+	myText := MyString(text)
+	vowels := "aeiou"
+	vowelsInString := myText.FilterChars(vowels)
+	fmt.Printf("There are %d vowels in thr provided text!\n", len(vowelsInString))
 }
 
 func pigLatin(sentence string) {
@@ -65,8 +86,6 @@ func pigLatinWord(word string) {
 		fmt.Print("You shouldn't be here!")
 	}
 }
-
-
 
 func reverseString(str string) {
 	temp := make([]string, len(str))
